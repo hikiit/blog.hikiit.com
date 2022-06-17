06c65647efea215cfb58d51c750dc3881a263066
hiknot <hiroktdev@gmail.com>
2022-06-18 00:50:05 +0900

    add plugin for Google Fonts

---
+++ src/utils/typography.js
@@ -0,0 +1,13 @@
+import Typography from "typography"
+
+const typography = new Typography({
+  googleFonts: [
+    {
+      name: "Noto Sans JP",
+      styles: ["400"],
+    },
+  ],
+  headerFontFamily: ["Noto Sans JP"],
+  bodyFontFamily: ["Noto Sans JP"],
+})
+export default typography
